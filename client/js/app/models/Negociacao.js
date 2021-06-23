bf721af5891e57015ac817cb7c6e27ae2bf2c206
iagolopess <iagoalopes@gmail.com>
2021-06-22 21:54:44 -0300

    metade do curso

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,15 +1,26 @@
 class Negociacao {
 
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this); // Não permite mais alterações a partir desse ponto
     }
 
-    obtemVolume(){
-        return this.quantidade * this.valor;
+    get volume(){
+        return this._quantidade * this._valor;
     }
 
+    get data(){
+        return new Date(this._data.getTime()); // Não permite que seja alterado a data
+    }
 
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
 
 }
