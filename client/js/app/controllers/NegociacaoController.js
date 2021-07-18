6f97526251fbdae4bc9e5f1909d3d4082e10b259
iagolopess <iagoalopes@gmail.com>
2021-07-18 16:02:51 -0300

    Acabou

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,10 +7,14 @@ class NegociacaoController{
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-    
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+
     }
 
     adiciona(event){
@@ -18,8 +22,11 @@ class NegociacaoController{
        
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
-        this._limpaFormulario();
-        
+
+        this._mensagem.texto = "Negociação adicionada com sucesso!";
+        this._mensagemView.update(this._mensagem);
+
+        this._limpaFormulario();        
     }
 
     _criaNegociacao(){
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento){
-        this._elemento = elemento;
+        super(elemento);
     }
 
-    _template(model){
+    template(model){
         return `
         <table class="table table-hover table-bordered">
         <thead>
@@ -37,30 +37,5 @@ class NegociacoesView {
     `;
     }
 
-    update(model){
-        this._elemento.innerHTML = this._template(model);
-    }
-
 }
 
-
-/*
-
- <table class="table table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-        </tbody>
-        
-        <tfoot>
-        </tfoot>
-    </table>
-
-*/
