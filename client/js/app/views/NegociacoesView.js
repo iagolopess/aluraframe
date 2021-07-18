ebfaf7b5b9f3750c73335a936b7bf0ffef91306b
iagolopess <iagoalopes@gmail.com>
2021-07-18 04:27:56 -0300

    Quase terminando

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -28,6 +28,10 @@ class NegociacoesView {
         </tbody>
         
         <tfoot>
+            <tr>
+                <td colspan="3"></td>
+                <td>${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td>
+            </tr>
         </tfoot>
     </table>
     `;
